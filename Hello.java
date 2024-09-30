7787f271e981af52d11f598eb4712f6d78cb4d8a
ViniciusRittono <vinicius.rittono@hotmail.com>
2024-09-30 11:12:07 -0300

    HelloWord_java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+    public static void main(String [] args) {
+        System.out.println("Hello Word");
+
+
+//complemento, só tava testando!!
+        int a = 6;
+        int b = 9;
+
+        System.out.println(a + b);
+    }
+}
